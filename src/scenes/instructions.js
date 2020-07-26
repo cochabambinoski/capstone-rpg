ee84d2ce5a15291959b1249102c40190e33fda21
Feber <cochabambinoski@gmail.com>
2020-07-25 23:04:06 -0400

    adding scenes

---
+++ src/scenes/instructions.js
@@ -0,0 +1,33 @@
+import Phaser from 'phaser';
+import Button from '../helpers/button';
+
+export default class InstructionsScene extends Phaser.Scene {
+  constructor() {
+    super('InstructionsScene');
+  }
+
+  create() {
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const instructionsText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 100,
+      text: '',
+      style: {
+        font: '30px monospace',
+        fill: '#ffffff',
+      },
+    });
+    instructionsText.setOrigin(0.5, 0.5);
+    instructionsText.setText(` 
+    Chichu Tales is a fighting game,
+    The fighting scene has two enemies
+    on the left and you also have two 
+    players on the right to fight against
+    the enemies. 
+    Use the space bar to attack your enemy
+    Also you use arrows to select who to 
+    attack.`);
+    this.menuButton = new Button(this, 400, 500, 'blueButton1', 'blueButton2', 'Menu', 'Title');
+  }
+}
